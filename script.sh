6002ac1bb9767cd350202c861236f9d91476994a
konoha <konohayuya1222@gmail.com>
2018-11-11 23:48:58 +0900

    fix script.sh it echo pandoc hogehoge but i want pandoc and build pdf file

--- script.sh
+++ script.sh
@@ -3,11 +3,11 @@
 for input_file in $(find */*.md )
 do
   output_file=$(echo $input_file | sed -e "s/.md/.pdf/"  |xargs basename)
-  echo "pandoc $input_file \
+  pandoc $input_file \
     -v $output_file \
     -H preamble.tex \
     --latex-engine=lualatex \
     -V documentclass=ltjarticle \
     -V geometry:a4paper \
-    --listings"
+    --listings
 done
