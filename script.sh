75a4c753c93754117b0cfc462f24ecfa77e105ce
konoha <konohayuya1222@gmail.com>
2018-11-13 17:23:29 +0900

    fix ミスがしょうもなさすぎる

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ do
   pandoc $input_file \
     -o $output_file \
     -H preamble.tex \
-    -r github_markdown
+    -r markdown_github \
     --latex-engine=lualatex \
     -V documentclass=ltjarticle \
     -V geometry:a4paper \
